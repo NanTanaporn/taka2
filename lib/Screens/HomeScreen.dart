60ad7e16598ecdca926f121557742a6ae37148b5
may <may@mays-MacBook-Pro.local>
2020-09-18 16:54:02 +0700

    first commit

---
+++ lib/Screens/HomeScreen.dart
@@ -0,0 +1,114 @@
+//import 'dart:html';
+import 'package:flutter/material.dart';
+import 'UserScreen.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  TextEditingController nameController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: ListView(
+        children: <Widget>[
+          _myCover(),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: TextField(
+                controller: nameController,
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.search),
+                  border: OutlineInputBorder(),
+                  labelText: 'ค้นหาสถานที่',
+                )),
+          ),
+          
+         SizedBox(height: 20.0),
+          Padding(
+            padding: EdgeInsets.only(top: 45.0),
+            child: Container(
+              height: MediaQuery.of(context).size.height - 300.0,
+              child: ListView(
+                children: [
+                // _myUser(),
+                // _myUser(),
+              ],
+            ),
+          ),
+          ),
+
+          SizedBox(height: 20.0),
+          Padding(
+            padding: EdgeInsets.only(bottom: 5.0),
+            child: Container(
+              child: Center(
+                child: MaterialButton(
+                  height: 40,
+                  minWidth: 250,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(20)),
+                  onPressed: () {
+                    // Navigator.push(context,
+                    // MaterialPageRoute(builder: (context) => MySignUp()));
+                  },
+                  child: Text(
+                    "กำหนดการเดินทางเอง",
+                    style: TextStyle(fontSize: 18, color: Colors.white),
+                  ),
+                  color: Color(0xFF7A9BEE),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+Widget _myCover() {
+  return Container(
+    height: 100,
+    decoration: BoxDecoration(
+        image: DecorationImage(
+            fit: BoxFit.cover, image: AssetImage('asset/img/header.jpg'))),
+    child: ListView(
+      children: <Widget>[
+        Positioned(
+            bottom: 20,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 40, top: 30),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  (Text(
+                    'TAKUSHI',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20.0),
+                  )),
+                  Container(
+                    width: 125.0,
+                    child: Row(
+                      children: [
+                        IconButton(
+                          icon: Icon(Icons.account_circle),
+                          iconSize: 43.0,
+                          color: Colors.white,
+                          onPressed: () {},
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            )),
+      ],
+    ),
+  );
+}
---
+++ lib/Screens/SignInScreen.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/gestures.dart';
+
+import 'SignUPScreen.dart';
+
+class SignInScreen extends StatefulWidget {
+  @override
+  _SignInScreenState createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: ListView(
+        children: <Widget>[
+          Container(
+            height: 300,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    fit: BoxFit.cover, image: AssetImage('asset/img/header.jpg'))),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Row(
+              children: <Widget>[
+                IconButton(icon: Icon(Icons.person), onPressed: null),
+                Expanded(
+                    child: Container(
+                        margin: EdgeInsets.only(right: 20, left: 10),
+                        child: TextField(
+                          decoration: InputDecoration(hintText: 'Username'),
+                        )))
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Row(
+              children: <Widget>[
+                IconButton(icon: Icon(Icons.lock), onPressed: null),
+                Expanded(
+                    child: Container(
+                        margin: EdgeInsets.only(right: 20, left: 10),
+                        child: TextField(
+                          decoration: InputDecoration(hintText: 'Password'),
+                        ))),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(5),
+              child: Container(
+                height: 60,
+                child: RaisedButton(
+                  onPressed: () {
+                        Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => SignUpScreen()));
+                      },
+                  color: Color(0xFF7A9BEE),
+                  child: Text(
+                    'SIGN IN',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
